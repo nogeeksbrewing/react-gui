974e84824058279044c48125b391691bb75e3256
Allan Lewis <nogeeks@Allans-Mac-mini.fios-router.home>
2019-07-20 22:04:32 -0400

    #4 - Brewery banner component

--- src/brewery-banner/BreweryBanner.tsx
+++ src/brewery-banner/BreweryBanner.tsx
@@ -5,60 +5,50 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 interface Props {
-    data: GetBreweryQuery;
+  data: GetBreweryQuery;
 }
 
 const useStyles = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
-  }));
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 const BreweryBanner: React.FC<Props> = ({ data }) => (
-        <div className={useStyles().heroContent}>
-          
-          <Container maxWidth="sm">
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                {data.brewery!.name}
-          </Typography>
-          
-            {/* <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                {data.brewery}
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                {data.brewery ? data.brewery.established : ""}
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                {data.brewery ? data.brewery.location : ""}
-            </Typography> */}
-            </Container>
-            </div>
+  <div className={useStyles().heroContent}>
+
+    <Container maxWidth="sm">
+      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+        {data.brewery!.name}
+      </Typography>
+    </Container>
+  </div>
 );
 
 export default BreweryBanner;
